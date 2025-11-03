cb3dee9b50ee65676c88c0c23a505d175afc4948
Hussein El-Morsy <husseinessa855@gmail.com>
2025-11-03 19:41:55 +0200

    Update rag_utility.py

--- rag_utility.py
+++ rag_utility.py
@@ -5,7 +5,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
-from langchain_classic.chains import RetrievalQA
+from langchain.chains import RetrievalQA
 
 #_ = load_dotenv(find_dotenv())
 
