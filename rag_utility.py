7f5888eac38c3c945e4597dad67b54972d54c00e
Hussein El-Morsy <hussein.el-morsy@ibm.com>
2025-11-03 11:32:48 +0200

    Implement Document processing to VectorDB

--- rag_utility.py
+++ rag_utility.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv, find_dotenv
 from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -6,6 +7,33 @@ from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
 from langchain_classic.chains import RetrievalQA
 
-
-
 _ = load_dotenv(find_dotenv())
+
+working_dir = os.path.dirname(os.path.abspath((__file__)))
+
+# load the embedding model
+embedding = HuggingFaceEmbeddings()
+
+# load the llm from groq
+llm = ChatGroq(
+    model='llama-3.3-70b-versatile',
+    temperature=0,
+)
+
+def process_document_to_chroma_db(file_name):
+    # load the pdfdocument
+    loader = UnstructuredFileLoader(f"{working_dir}/{file_name}")
+    documents = loader.load()
+    # split the text into chunks
+    text_spilitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=200,
+    )
+    texts = text_spilitter.split_documents(documents)
+    # store document chunks in the chroma database
+    vectordb = Chroma.from_documents(
+        documents=texts,
+        embedding=embedding,
+        persist_directory=f"{working_dir}/doc_vectordb",
+    )
+    return 0;
