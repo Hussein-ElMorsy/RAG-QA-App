ec1605a78d630920ab022a46b1968362a7bccae7
Hussein El-Morsy <hussein.el-morsy@ibm.com>
2025-11-03 10:37:03 +0200

    Add required file

---
+++ rag_utility.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv, find_dotenv
+from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_groq import ChatGroq
+from langchain_classic.chains import RetrievalQA
+
+
+
+_ = load_dotenv(find_dotenv())
