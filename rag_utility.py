80650cc8183120079fd4af9af2ae8e600c997728
Hussein El-Morsy <husseinessa855@gmail.com>
2026-01-12 10:26:04 +0200

    Update rag_utility.py
    fix error

--- rag_utility.py
+++ rag_utility.py
@@ -5,7 +5,10 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_groq import ChatGroq
-from langchain.chains import RetrievalQA
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
+
 
 _ = load_dotenv(find_dotenv())
 
@@ -36,23 +39,41 @@ def process_document_to_chroma_db(file_name):
         embedding=embedding,
         persist_directory=f"{working_dir}/doc_vectordb",
     )
-    return 0;
+    vectordb.persist()
+    return True;
 
 def answer_question(user_prompt):
-    # load the presistent chroma vector database
     vectordb = Chroma(
         persist_directory=f"{working_dir}/doc_vectordb",
         embedding_function=embedding,
     )
-    # create a retriever for document search
+
     retriever = vectordb.as_retriever()
-    # create a retriever chain to ansswer user questions
-    qa_chain = RetrievalQA.from_chain_type(
+
+    prompt = ChatPromptTemplate.from_template("""
+    Answer the question using only the provided context.
+
+    Context:
+    {context}
+
+    Question:
+    {input}
+    """)
+
+    combine_docs_chain = create_stuff_documents_chain(
         llm=llm,
-        chain_type="stuff",
+        prompt=prompt
+    )
+
+    retrieval_chain = create_retrieval_chain(
         retriever=retriever,
-    ) 
-    response = qa_chain.invoke({"query": user_prompt})
-    answer = response["result"]
-    return answer
+        combine_docs_chain=combine_docs_chain
+    )
+
+    response = retrieval_chain.invoke({
+        "input": user_prompt
+    })
+
+    return response["answer"]
+
 
